414854a24b7989f957c4c8f7085faf24470d06f3
Emile Wong <emilewong@outlook.com>
2021-06-28 16:25:38 +0800

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Emile Wong on 28/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
